a3167cab0c82ecce30670f939a09469620c03594
den3 <joe199194@gmail.com>
2016-03-11 18:49:46 +0800

    fix comment

--- message/pubrec.go
+++ message/pubrec.go
@@ -3,6 +3,8 @@ package message
 // PubrecMessage is that a PUBREC Packet is the response to a PUBLISH Packet with QoS 2.
 // It is the second packet of the QoS 2 protoocl exchange.
 type PubrecMessage struct {
+	// Remaining Length field
+	// This is the length of the variable header. For the PUBREC Packet this has the value 2.
 	fixedHeader
 
 	// This contains the Packet Identifier from the PUBLISH Packet that is bening
