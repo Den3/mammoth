ec10eb5770d93a71c9697c33cd12786691c97bb7
den3 <joe199194@gmail.com>
2016-03-26 23:14:14 +0800

    defined interface for message

--- message/message.go
+++ message/message.go
@@ -1,1 +1,14 @@
 package message
+
+import "io"
+
+// Message is the interface defined for all MQTT Control types
+type Message interface {
+	Write(io.Writer) error
+	Read(io.Reader) error
+}
+
+// NewMessageByFixedHeader returns Control Packet by Fixed Header
+func NewMessageByFixedHeader(fh *fixedHeader) Message {
+	return nil
+}
--- message/message_test.go
+++ message/message_test.go
@@ -1,1 +1,7 @@
 package message
+
+import "testing"
+
+func TestNewMessageByFixedHeader(t *testing.T) {
+	t.Error("TestNewMessageByFixedHeader failed")
+}
